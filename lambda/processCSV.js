3f711f0bfcf0f5169fb83693f33a863a8cd93bdf
HussainShah1551 <168480150+HussainShah1551@users.noreply.github.com>
2025-05-02 21:35:24 +0500

    Update processCSV.js
    "Check Flag "

--- lambda/processCSV.js
+++ lambda/processCSV.js
@@ -21,7 +21,7 @@ exports.handler = async (event, context) => {
   }
   
   // Check if email replacement is enabled
-  const replaceEmails = process.env.REPLACE_EMAILS === 'false';
+  const replaceEmails = process.env.REPLACE_EMAILS === 'true'; // set to false to send emails to orignal emails 
   console.log(`Email replacement is ${replaceEmails ? 'enabled' : 'disabled'}`);
   
 
